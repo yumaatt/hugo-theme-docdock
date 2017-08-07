1141cdef13afdfb9fdfb2348df070d89f782b2b4
Julien Vernet <julien@vernet.me>
2017-08-07 21:20:36 +0700

    Move image related functions to DOM ready

--- static/js/hugo-learn.js
+++ static/js/hugo-learn.js
@@ -16,48 +16,50 @@ var getUrlParameter = function getUrlParameter(sPageURL) {
     }
 };
 
-// Execute actions on images generated from Markdown pages
-var images = $("div#body-inner img").not(".inline");
-// Wrap image inside a featherlight (to get a full size view in a popup)
-images.wrap(function () {
-    var image = $(this);
-    if (!image.parent("a").length) {
-        return "<a href='" + image[0].src + "' data-featherlight='image'></a>";
-    }
-});
+jQuery(document).ready(function () {
 
-// Change styles, depending on parameters set to the image
-images.each(function (index) {
-    var image = $(this)
-    var o = getUrlParameter(image[0].src);
-    if (typeof o !== "undefined") {
-        var h = o["height"];
-        var w = o["width"];
-        var c = o["classes"];
-        image.css("width", function () {
-            if (typeof w !== "undefined") {
-                return w;
-            } else {
-                return "auto";
-            }
-        });
-        image.css("height", function () {
-            if (typeof h !== "undefined") {
-                return h;
-            } else {
-                return "auto";
-            }
-        });
-        if (typeof c !== "undefined") {
-            var classes = c.split(',');
-            for (i = 0; i < classes.length; i++) {
-                image.addClass(classes[i]);
+    // Execute actions on images generated from Markdown pages
+    var images = $("div#body-inner img").not(".inline");
+    
+    // Wrap image inside a featherlight (to get a full size view in a popup)
+    images.wrap(function () {
+        var image = $(this);
+        if (!image.parent("a").length) {
+            return "<a href='" + image[0].src + "' data-featherlight='image'></a>";
+        }
+    });
+
+    // Change styles, depending on parameters set to the image
+    images.each(function (index) {
+        var image = $(this)
+        var o = getUrlParameter(image[0].src);
+        if (typeof o !== "undefined") {
+            var h = o["height"];
+            var w = o["width"];
+            var c = o["classes"];
+            image.css("width", function () {
+                if (typeof w !== "undefined") {
+                    return w;
+                } else {
+                    return "auto";
+                }
+            });
+            image.css("height", function () {
+                if (typeof h !== "undefined") {
+                    return h;
+                } else {
+                    return "auto";
+                }
+            });
+            if (typeof c !== "undefined") {
+                var classes = c.split(',');
+                for (i = 0; i < classes.length; i++) {
+                    image.addClass(classes[i]);
+                }
             }
         }
-    }
-});
+    });
 
-jQuery(document).ready(function () {
     // Add link button for every
     var text, clip = new Clipboard('.anchor');
     $("h1~h2,h1~h3,h1~h4,h1~h5,h1~h6").append(function (index, html) {
